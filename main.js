36351894113cec89084c5bb7063f89c999a474fb
Abdulrahman-M-Darwish <winding192837456456@gmail.com>
2023-01-20 22:45:31 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll(".slides");
+const dots = document.querySelectorAll(".dot");
+
+function setActive(i) {
+  for (const slide of slides) {
+    slide.classList.remove("active");
+    slides[i].classList.add("active");
+  }
+  for (const dot of dots) {
+    dot.classList.remove("active");
+    dots[i].classList.add("active");
+  }
+}
+
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", function () {
+    setActive(i);
+  });
+}
